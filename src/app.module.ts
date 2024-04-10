5f32488a3af00f7c3bedc6f4ca96bc36fc67dd56
Kim Alex <kimalex2004dev@gmail.com>
2024-04-11 02:38:34 +0900

    Removed useless code

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PromptingModule } from './prompting/prompting.module';
-import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
-  imports: [
-    PromptingModule,
-    MulterModule.register({
-      dest: './files',
-    }),
-  ],
+  imports: [PromptingModule],
   controllers: [AppController],
   providers: [AppService],
 })
