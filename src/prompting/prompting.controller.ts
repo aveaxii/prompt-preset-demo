e940252487d749207ff356527cf47991e77200b0
Kim Alex <kimalex2004dev@gmail.com>
2024-04-12 15:10:29 +0900

    Endpoints: SD 1.6, SDXL 1.0, Inpaint and Outpaint (restv2)

--- src/prompting/prompting.controller.ts
+++ src/prompting/prompting.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  Get,
   Post,
   Res,
   UploadedFile,
@@ -14,16 +15,16 @@ import { Response } from 'express';
 export class PromptingController {
   constructor(private readonly promptingService: PromptingService) {}
 
-  @Post('preset')
+  @Post('preset-16')
   @UseInterceptors(FileInterceptor('image'))
-  async getPrompting(
+  async getPrompting16(
     @Body('userPrompt') userPrompt: string,
     @Body('preset') preset: string,
     @Body('weight') weight: number,
     @UploadedFile() image: Express.Multer.File,
     @Res() res: Response,
   ) {
-    const imageBuffer = await this.promptingService.generateImage(
+    const imageBuffer = await this.promptingService.imageToImageV16(
       userPrompt,
       preset,
       weight,
@@ -34,5 +35,78 @@ export class PromptingController {
     res.setHeader('Content-Length', imageBuffer.length.toString());
 
     res.end(imageBuffer);
+  }
+
+  @Post('preset-XL')
+  @UseInterceptors(FileInterceptor('image'))
+  async getPromptingXL(
+    @Body('userPrompt') userPrompt: string,
+    @Body('preset') preset: string,
+    @Body('weight') weight: number,
+    @UploadedFile() image: Express.Multer.File,
+    @Res() res: Response,
+  ) {
+    const imageBuffer = await this.promptingService.imageToImageXL(
+      userPrompt,
+      preset,
+      weight,
+      image,
+    );
+
+    res.setHeader('Content-Type', 'image/jpeg');
+    res.setHeader('Content-Length', imageBuffer.length.toString());
+
+    res.end(imageBuffer);
+  }
+
+  @Post('preset-v2')
+  @UseInterceptors(FileInterceptor('image'))
+  async getPromptingV2(
+    @Res() res: Response,
+    @UploadedFile() image: Express.Multer.File,
+    @Body('userPrompt') userPrompt: string,
+    @Body('preset') preset: string,
+    @Body('outputFormat') outputFormat: string,
+    @Body('negativePrompt') negativePrompt: string,
+  ) {
+    const imageBuffer = await this.promptingService.inpaintV2(
+      image,
+      userPrompt,
+      preset,
+      outputFormat,
+      negativePrompt,
+    );
+
+    res.setHeader('Content-Type', 'image/jpeg');
+    res.setHeader('Content-Length', imageBuffer.length.toString());
+
+    res.end(imageBuffer);
+  }
+
+  @Post('outpaint')
+  @UseInterceptors(FileInterceptor('image'))
+  async outpaint(
+    @Res() res: Response,
+    @UploadedFile() image: Express.Multer.File,
+    @Body('userPrompt') userPrompt: string,
+    @Body('preset') preset: string,
+    @Body('outputFormat') outputFormat: string,
+  ) {
+    const imageBuffer = await this.promptingService.outpaint(
+      image,
+      userPrompt,
+      preset,
+      outputFormat,
+    );
+
+    res.setHeader('Content-Type', 'image/jpeg');
+    res.setHeader('Content-Length', imageBuffer.length.toString());
+
+    res.end(imageBuffer);
+  }
+
+  @Get('engines')
+  async getEngines() {
+    return await this.promptingService.getListOfEngines();
   }
 }
